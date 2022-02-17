63df36ab0f776c5e8468c3d1dbbe9288f3a1a54e
Jeonghun Kim <jhk91@yonsei.ac.kr>
2022-02-17 20:13:44 +0900

    20220217_pj1

---
+++ CamelCase/CamelCase/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace CamelCase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtQRCode = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "QR Code";
+            // 
+            // pic
+            // 
+            this.pic.BackColor = System.Drawing.Color.White;
+            this.pic.Location = new System.Drawing.Point(16, 34);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(411, 292);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pic.TabIndex = 1;
+            this.pic.TabStop = false;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(352, 415);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "QR 생성";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtQRCode
+            // 
+            this.txtQRCode.Location = new System.Drawing.Point(16, 332);
+            this.txtQRCode.Multiline = true;
+            this.txtQRCode.Name = "txtQRCode";
+            this.txtQRCode.Size = new System.Drawing.Size(411, 77);
+            this.txtQRCode.TabIndex = 3;
+            this.txtQRCode.Text = "https://papago.naver.com/";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(439, 439);
+            this.Controls.Add(this.txtQRCode);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QR Code 생성기";
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtQRCode;
+    }
+}
+
---
+++ CamelCase/CamelCase/Form1.cs
@@ -0,0 +1,56 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace CamelCase
+{
+    public partial class Form1 : Form
+    {
+        private string sentence;
+        private SpeechSynthesizer synthesizer;
+
+        interface form
+        {
+            //추상화로 구조 짤 것!
+            string strCodeLink { get; set; }
+            string strQRLink { get; set; }
+            string sentence { get; set; }
+
+            SpeechSynthesizer synthesizer { get; set; }
+
+            void btnGenerate_Click(object sender, EventArgs e);
+            void SpeechSynthesize(string sentence);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            sentence = "다음으로  이동합니다.";
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            QRCodeGenerator qGen = new QRCodeGenerator();
+            QRCodeData qData = qGen.CreateQrCode(txtQRCode.Text, QRCodeGenerator.ECCLevel.Q);
+            QRCode qCode = new QRCode(qData);
+            pic.Image = qCode.GetGraphic(7);
+            SpeechSynthesize(sentence);
+        }
+
+        private void SpeechSynthesize(string sentence)
+        {
+            synthesizer = new SpeechSynthesizer();
+            synthesizer.SetOutputToDefaultAudioDevice();
+            synthesizer.SelectVoice("Microsoft Heami Desktop");
+            synthesizer.Speak(sentence);
+        }
+    }
+}
