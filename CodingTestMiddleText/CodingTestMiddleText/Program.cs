f4ccdc36ce1274b0f2db5a29990a3e0b0413da81
Jeonghun Kim <jhk91@yonsei.ac.kr>
2022-02-24 00:00:06 +0900

    20220223

---
+++ CodingTestMiddleText/CodingTestMiddleText/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+public class Solution
+{
+    public static void Main()
+    {
+        Solution solution = new Solution(); 
+    }
+    public string solution(string s)
+    {
+        string answer = "";
+        if (s.Length < 3)
+        {
+            answer = s;
+        }
+        else
+        {
+            int nLength = (int)s.Length / 2;
+            if (s.Length % 2 == 0)
+            {
+                answer = s[nLength - 1].ToString() + s[nLength].ToString();
+            }
+            else
+            {
+                answer = s[nLength].ToString();
+            }
+        }
+        return answer;
+    }
+}
